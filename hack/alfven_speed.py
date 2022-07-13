b46886c3315fbd0522a0d34b0408532002100ab5
johntkappel <johntkappel@gmail.com>
2022-07-13 14:47:59 -0400

    fixed a math error

--- hack/alfven_speed.py
+++ hack/alfven_speed.py
@@ -11,4 +11,4 @@ import astropy.units as u
 from astropy.constants.si import k_B, mu0
 
 def Alfven_speed(B: u.T, density: (u.kg / u.m**3)) -> u.m / u.s :
-    return np.abs(B)/ np.sqrt(mu0/density)
+    return np.abs(B)/ np.sqrt(mu0*density)
